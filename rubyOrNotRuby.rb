0f396c9e27b83ead4b0460019db22c3d15361270
Michel Frechette <mifrec@hotmail.com>
2018-01-09 11:22:45 -0500

    5 main arithmetic operators concatenation

--- rubyOrNotRuby.rb
+++ rubyOrNotRuby.rb
@@ -1,7 +1,14 @@
 # Comment
-print "Enter a Value: "
-first_num = gets.to_i
-print "Enter Another Value: "
-second_num = gets.to_i
+# print "Enter a Value: "
+# first_num = gets.to_i
+# print "Enter Another Value: "
+# second_num = gets.to_i
 # puts first_num.to_s + " + " + second_num.to_s + " = " + (first_num + second_num).to_s
-puts "#{first_num} + #{second_num} = #{first_num + second_num}"
+# puts "#{first_num} + #{second_num} = #{first_num + second_num}"
+
+# Five Main Arithmetic Operators
+puts "6 + 4 = " + (6+4).to_s
+puts "6 - 4 = " + (6-4).to_s
+puts "6 * 4 = " + (6*4).to_s
+puts "6 / 4 = " + (6/4).to_s
+puts "6 % 4 = " + (6%4).to_s
